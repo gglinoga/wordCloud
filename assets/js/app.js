19c878bf1f80d5d123278d22448b2b8f0051be40
Greg Glinoga <gglinoga@gmail.com>
2019-08-02 19:45:46 -0700

    n

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,11 @@ let args = process.argv;
 let type = args[2];
 let arr = [];
 let bufferString;
+var jsonObj = [];
+var headers;
+    wait = () =>{console.log(obj)}
 
-csvHandler = (req, res) => {
+csvHandler = () => {
 
     let obj = fs.readFile('../data/words.csv', (err, data) => {
         if (err) throw err;
@@ -12,14 +15,15 @@ csvHandler = (req, res) => {
         bufferString = data.toString();
         arr = bufferString.split('\n');
 
+        console.log('test')
         // console.log(arr);
 
         for (i = 0; i < arr.length; i++) {
             JSON.stringify(arr[i]);
         }
 
-        var jsonObj = [];
         var headers = arr[0].split(',');
+
     
         for (var i = 1; i < arr.length; i++) {
             var data = arr[i].split(',');
@@ -30,11 +34,10 @@ csvHandler = (req, res) => {
             jsonObj.push(obj);
         }
         JSON.stringify(jsonObj);
+        // console.log(jsonObj)
         // console.log(jsonObj);
         return jsonObj;
-
     })
-return obj
 }
 
-console.log(csvHandler)
+csvHandler();
